0a1489c6034a16d5be43c043773c4505127bd999
MagdaCodes <themsway123@gmail.com>
2022-04-09 17:13:06 +0200

    styling changes

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ function displayTemperature(response) {
 
   celsiusTemperature = response.data.main.temp;
 
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
